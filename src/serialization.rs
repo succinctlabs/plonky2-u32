1410b3b5cad5af98d119862ecff39efe3221bffd
Tamir Hemo <tamir@succinct.xyz>
2023-07-21 15:56:01 -0700

    lifetime

--- src/serialization.rs
+++ src/serialization.rs
@@ -18,7 +18,7 @@ pub trait ReadU32 {
     fn read_target_u32(&mut self) -> IoResult<U32Target>;
 }
 
-impl<'a> ReadU32 for Buffer<'a> {
+impl ReadU32 for Buffer<'_> {
     #[inline]
     fn read_target_u32(&mut self) -> IoResult<U32Target> {
         Ok(U32Target(self.read_target()?))
